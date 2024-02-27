f7aa247c6344846e3bd0cf8b71d312c7168cf7a1
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2024-02-27 23:49:36 +0200

    Remove AlohaKit from 3rd party sample

--- samples/ThirdPartyControlsSample/Properties/Elements.cs
+++ samples/ThirdPartyControlsSample/Properties/Elements.cs
@@ -1,22 +1,6 @@
-﻿using AlohaKit.Controls;
-using BlazorBindings.Maui.ComponentGenerator;
+﻿using BlazorBindings.Maui.ComponentGenerator;
 using SkiaSharp.Views.Maui.Controls;
 using XCalendar.Maui.Views;
-
-// AlohaKit.Controls
-[assembly: GenerateComponent(typeof(Rating))]
-[assembly: GenerateComponent(typeof(BusyIndicator))]
-[assembly: GenerateComponent(typeof(Avatar))]
-[assembly: GenerateComponent(typeof(AlohaKit.Controls.Button), Aliases = new[] { "Button:AlhButton" })]
-[assembly: GenerateComponent(typeof(AlohaKit.Controls.CheckBox), Aliases = new[] { "CheckBox:AlhCheckBox" })]
-[assembly: GenerateComponent(typeof(AlohaKit.Controls.ProgressBar), Aliases = new[] { "ProgressBar:AlhProgressBar" })]
-[assembly: GenerateComponent(typeof(NumericUpDown))]
-[assembly: GenerateComponent(typeof(ProgressRadial))]
-[assembly: GenerateComponent(typeof(ToggleSwitch),
-    Exclude = new[] { nameof(ToggleSwitch.Toggled) },
-    PropertyChangedEvents = new[] { nameof(ToggleSwitch.IsOn) })]
-[assembly: GenerateComponent(typeof(AlohaKit.Controls.Slider), Aliases = new[] { "Slider:AlhSlider" })]
-[assembly: GenerateComponent(typeof(PulseIcon))]
 
 // CommunityToolkit
 [assembly: GenerateComponent(typeof(CommunityToolkit.Maui.Views.AvatarView), Exclude = new[] { nameof(CommunityToolkit.Maui.Views.AvatarView.CornerRadius) })]
