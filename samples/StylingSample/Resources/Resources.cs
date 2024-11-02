d992895da01a61d3a2f4caf38e43c88ae19dc216
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2024-11-02 16:27:52 +0200

    Styling sample

---
+++ samples/StylingSample/Resources/Resources.cs
@@ -0,0 +1,8 @@
+﻿using StylingSample.Resources.Styles;
+
+namespace StylingSample.Resources;
+
+public static class Resources
+{
+    public static readonly ResourceDictionary ScopedStyle = new ScopedStyle();
+}
---
+++ samples/StylingSample/Resources/Styles/ScopedStyle.xaml.cs
@@ -0,0 +1,9 @@
+namespace StylingSample.Resources.Styles;
+
+public partial class ScopedStyle : ResourceDictionary
+{
+	public ScopedStyle()
+	{
+		InitializeComponent();
+	}
+}
