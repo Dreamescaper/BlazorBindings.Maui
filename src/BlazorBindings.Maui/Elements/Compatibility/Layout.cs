5f858c05fa966cf2977c433383848f3994e3fb63
Oleksandr Liakhevych <liakh.oleksandr32@gmail.com>
2024-11-13 23:53:41 +0200

    Add deprecated attributes for Frame and Compatibility.Layout

---
+++ src/BlazorBindings.Maui/Elements/Compatibility/Layout.cs
@@ -0,0 +1,5 @@
+﻿namespace BlazorBindings.Maui.Elements.Compatibility;
+
+
+[Obsolete("Use Microsoft.Maui.Controls.Layout instead. For more information, see https://learn.microsoft.com/dotnet/maui/user-interface/layouts/custom")]
+public partial class Layout;
---
+++ src/BlazorBindings.Maui/Elements/Frame.cs
@@ -0,0 +1,4 @@
+﻿namespace BlazorBindings.Maui.Elements;
+
+[Obsolete("Frame is obsolete as of .NET 9. Please use Border instead.")]
+public partial class Frame;
